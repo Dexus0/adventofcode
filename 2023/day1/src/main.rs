6fead99ef15bf1c96e2c7ac4edeca7fa11df4bf9
Duncan Vriend <vriendduncan@gmail.com>
2023-12-04 17:11:26 +0100

    day 1 part 2 "functional"
    It's functional, but according to the site invalid.
    Still need to make tests to see if it works with examples.

--- 2023/day1/src/main.rs
+++ 2023/day1/src/main.rs
@@ -1,25 +1,24 @@
-use std::ffi::OsStr;
+use regex::bytes::Regex;
 use std::io::{ErrorKind, Result};
 
 fn main() -> Result<()> {
     use std::env::args_os;
+    use std::fs::read;
 
     let input = args_os().nth(1).unwrap_or_default(); // 0th is a garbage/undefined value
 
-    let result = process_file(&input)?;
+    let result = process_data(read(input)?)?;
 
     println!("{result}");
 
     Ok(())
 }
 
-fn process_file(file: &OsStr) -> Result<usize> {
-    use std::fs::read;
+fn process_data(mut rawdata: Vec<u8>) -> Result<usize> {
     use std::io::Error;
 
-    let rawdata = read(file)?;
-
     let calibration_file = if rawdata.is_ascii() {
+        rawdata.make_ascii_lowercase();
         rawdata
     } else {
         return Err(Error::new(
@@ -30,45 +29,73 @@ fn process_file(file: &OsStr) -> Result<usize> {
 
     let calibration_lines = calibration_file.split(|x| *x == b'\n');
 
-    let mut calibration_sum = 0;
+    let mut calibration_sum: usize = 0;
 
     for line in calibration_lines {
-        calibration_sum += calculate_calibration_value(line);
+        // line needs to be valid ASCII to get here
+        calibration_sum += calculate_calibration_value(line) as usize;
     }
     Ok(calibration_sum)
 }
 
-fn calculate_calibration_value(line: &[u8]) -> usize {
-    const ZERO: u8 = b'0';
-    const NINE: u8 = b'9';
+/// # Usage
+/// Assumes input is lowercase ASCII
+fn calculate_calibration_value(line: &[u8]) -> u8 {
+    use std::str;
+    let regex = unsafe {
+        // This should be valid regex
+        Regex::new(r"[1-9]|one|two|three|four|five|six|seven|eight|nine").unwrap_unchecked()
+    };
+
+    let mut iter = regex.find_iter(line);
+
+    unsafe {
+        print!("{} ", str::from_utf8_unchecked(line));
+    }
+
+    let first = if let Some(matched) = iter.next() {
+        unsafe {
+            print!("{}", str::from_utf8_unchecked(matched.as_bytes()));
+        }
+        ascii_to_value(matched.as_bytes())
+    } else {
+        0
+    };
+
+    let last = if let Some(matched) = iter.last() {
+        unsafe {
+            print!("{}", std::str::from_utf8_unchecked(matched.as_bytes()));
+        }
+        ascii_to_value(matched.as_bytes())
+    } else {
+        first
+    };
+
+    print!(" {first}{last} ");
+
+    let result = first * 10 + last;
+    println!("{result}");
+    result
+}
+
+/// # Usage
+/// Assumes input is lowercase ASCII
+fn ascii_to_value(str: &[u8]) -> u8 {
     const ASCII_MASK: u8 = 0x0F;
 
-    let mut first = 0;
-    let mut last = 0;
-
-    for char in line {
-        match *char {
-            ZERO..=NINE => {
-                first = *char;
-                first &= ASCII_MASK;
-                break;
-            }
-            _ => continue,
-        }
+    match str {
+        b"one" => 1,
+        b"two" => 2,
+        b"three" => 3,
+        b"four" => 4,
+        b"five" => 5,
+        b"six" => 6,
+        b"seven" => 7,
+        b"eight" => 8,
+        b"nine" => 9,
+        str => match str[0] {
+            char @ b'1'..=b'9' => char & ASCII_MASK,
+            _ => 0,
+        },
     }
-
-    for char in line.iter().rev() {
-        match *char {
-            ZERO..=NINE => {
-                last = *char;
-                last &= ASCII_MASK;
-                break;
-            }
-            _ => continue,
-        }
-    }
-
-    println!("{first}{last}");
-
-    (first * 10 + last) as usize
 }
