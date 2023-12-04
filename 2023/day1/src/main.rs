e413a6ca7280960d21bcb80f70ed6e1b841a6fce
Duncan Vriend <vriendduncan@gmail.com>
2023-12-05 00:25:02 +0100

    added tests using the site's examples

--- 2023/day1/src/main.rs
+++ 2023/day1/src/main.rs
@@ -99,3 +99,37 @@ fn ascii_to_value(str: &[u8]) -> u8 {
         },
     }
 }
+
+#[cfg(test)]
+mod examples {
+    use super::process_data;
+    #[test]
+    fn part1() {
+        let rawdata = "1abc2
+pqr3stu8vwx
+a1b2c3d4e5f
+treb7uchet"
+            .as_bytes()
+            .to_owned();
+
+        let result = process_data(rawdata).unwrap_or_default();
+
+        assert_eq!(result, 142);
+    }
+
+    #[test]
+    fn part2() {
+        let rawdata = "two1nine
+eightwothree
+abcone2threexyz
+xtwone3four
+4nineeightseven2
+zoneight234
+7pqrstsixteen"
+            .as_bytes()
+            .to_owned();
+        let result = process_data(rawdata).unwrap_or_default();
+
+        assert_eq!(result, 281);
+    }
+}
